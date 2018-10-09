09599deb11fa7e0037944a036039fa4258cf9bb5
Seth Bailey <seth.bailey@gm.com>
2018-10-09 09:29:48 -0500

    First commit

---
+++ iBeacon/View Controllers/AddItemViewController.swift
@@ -0,0 +1,104 @@
+//
+//  AddItemViewController.swift
+//  iBeacon
+//
+//  Created by Seth Bailey on 9/21/18.
+//  Copyright © 2018 Seth Bailey. All rights reserved.
+//
+/*
+ * Copyright (c) 2017 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+
+protocol AddBeacon {
+    func addBeacon(item: IBeaconItem)
+}
+
+class AddItemViewController: UIViewController {
+    
+    @IBOutlet weak var textName: UITextField!
+    @IBOutlet weak var txtUUID: UITextField!
+    @IBOutlet weak var txtMajor: UITextField!
+    @IBOutlet weak var txtMinor: UITextField!
+    @IBOutlet weak var btnAdd: UIButton!
+    
+    let uuidRegex = try! NSRegularExpression(pattern: "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", options: .caseInsensitive)
+    
+    var delegate: AddBeacon?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        btnAdd.isEnabled = false
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // Dismiss keyboard
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func textFieldEditingChanged(_ sender: UITextField) {
+        // Is name valid?
+        let nameValid = (textName.text!.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).characters.count > 0)
+        
+        // Is UUID valid?
+        var uuidValid = false
+        let uuidString = txtUUID.text!.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        if uuidString.characters.count > 0 {
+            uuidValid = (uuidRegex.numberOfMatches(in: uuidString, options: [], range: NSMakeRange(0, uuidString.characters.count)) > 0)
+        }
+        txtUUID.textColor = (uuidValid) ? .black : .red
+        
+        // Toggle btnAdd enabled based on valid user entry
+        btnAdd.isEnabled = (nameValid && uuidValid)
+    }
+    
+    @IBAction func btnAdd_Pressed(_ sender: UIButton) {
+        // Create new beacon item
+        let uuidString = txtUUID.text!.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        guard let uuid = UUID(uuidString: uuidString) else { return }
+        let major = Int(txtMajor.text!) ?? 0
+        let minor = Int(txtMinor.text!) ?? 0
+        let name = textName.text!.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        
+        let newItem = IBeaconItem(name: name, uuid: uuid, majorValue: major, minorValue: minor)
+        
+        delegate?.addBeacon(item: newItem)
+        print("Saved Item")
+        dismiss(animated: true, completion: nil)
+        
+        _ = navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func btnCancel_Pressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension AddItemViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Enter key hides keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+}
